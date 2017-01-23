6a8bd0fbe9fbf2e411f0a824fe0181e60578e432
Sebastian Bodzak <sebastian.bodzak@impaqgroup.com>
2017-01-23 14:46:47 +0100

    base template

--- src/main/java/pl/com/sebastianbodzak/configuration/security/Security.java
+++ src/main/java/pl/com/sebastianbodzak/configuration/security/Security.java
@@ -3,6 +3,7 @@ package pl.com.sebastianbodzak.configuration.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,5 +21,13 @@ public class Security extends WebSecurityConfigurerAdapter {
                 .withUser("user").password("pass").roles("USER");
     }
 
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/css/**", "/index").permitAll()
+                    .antMatchers("/user/**").hasRole("USER")
+                    .and().formLogin()
+                    .loginPage("/login").failureUrl("/login-error")
+                    .and().httpBasic();
+    }
 }
--- src/main/java/pl/com/sebastianbodzak/configuration/security/SecurityInitializer.java
+++ src/main/java/pl/com/sebastianbodzak/configuration/security/SecurityInitializer.java
@@ -1,7 +1,9 @@
 package pl.com.sebastianbodzak.configuration.security;
 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
 /**
  * Created by sbod on 23.01.17.
  */
-public class SecurityInitializer {
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 }
