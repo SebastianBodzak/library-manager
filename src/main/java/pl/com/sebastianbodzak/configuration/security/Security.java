79981d2635d1f936b5bab9c122cfdb1a4cf6f488
Sebastian Bodzak <sebastian.bodzak@impaqgroup.com>
2017-01-23 14:28:20 +0100

    base template

---
+++ src/main/java/pl/com/sebastianbodzak/configuration/security/Security.java
@@ -0,0 +1,24 @@
+package pl.com.sebastianbodzak.configuration.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by sbod on 23.01.17.
+ */
+@Configuration
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("pass").roles("USER");
+    }
+
+
+}
---
+++ src/main/java/pl/com/sebastianbodzak/configuration/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package pl.com.sebastianbodzak.configuration.security;
+
+/**
+ * Created by sbod on 23.01.17.
+ */
+public class SecurityInitializer {
+}
